1e8f0fe001ecd1cb4f5b7083d2230a27f7aeece1
leonardo <leonardo2013ufs@gmail.com>
2018-03-04 00:20:11 -0300

    Com a implementação de gráfico

--- CajuBio/CajuBio/View/ViewIndicadorAcesso.xaml.cs
+++ CajuBio/CajuBio/View/ViewIndicadorAcesso.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SkiaSharp;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,7 +15,34 @@ namespace CajuBio.View
 	{
 		public ViewIndicadorAcesso ()
 		{
-			InitializeComponent ();
-		}
-	}
+            Grafico.Chart = new Microcharts.BarChart() { Entries = entries };
+        }
+        List<Microcharts.Entry> entries = new List<Microcharts.Entry>
+        {
+            new Microcharts.Entry(200)
+            {
+                Label = "Médico Residente",
+                ValueLabel = "200",
+                Color = SKColor.Parse("#266489")
+            },
+            new Microcharts.Entry(250)
+            {
+                Label = "Enfermeiro",
+                ValueLabel = "250",
+                Color = SKColor.Parse("#68B9C0")
+            },
+            new Microcharts.Entry(100)
+            {
+                Label = "Médico Professor",
+                ValueLabel = "100",
+                Color = SKColor.Parse("#90D585")
+            },
+            new Microcharts.Entry(150)
+            {
+                Label = "Enfermeiro",
+                ValueLabel = "150",
+                Color = SKColor.Parse("#e77e23")
+            }
+        };
+    }
 }
